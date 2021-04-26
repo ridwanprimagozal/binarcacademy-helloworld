69d57bf4942ca88c96ec38b1ddcbafbe500de898
ridwanprimagozal <ridwanprimagozzal@gmail.com>
2021-04-26 22:41:53 +0700

    creating fitur rectangle

---
+++ src/main/kotlin/NumberTriangle.kt
@@ -0,0 +1,11 @@
+class NumberTriangle {
+    fun createNumberTriangle(maxNumber:Int){
+        for(i in 1..maxNumber){
+            for (j in 1..1){
+                println(j)
+            }
+            println()
+        }
+
+    }
+}
