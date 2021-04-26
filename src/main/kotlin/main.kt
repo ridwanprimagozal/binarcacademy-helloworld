a5ecf0a71b6201afcc24e1bb0ebfeb5fe280ffae
ridwanprimagozal <ridwanprimagozzal@gmail.com>
2021-04-26 23:14:58 +0700

    create header app

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,6 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    println("===================================")
+    println("======Cetak Segitiga Angka=========")
+    println("===================================")
 }
+
